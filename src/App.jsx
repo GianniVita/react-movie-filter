3cdf49a89d5dd9ae86618df5ebf524fa7cd13eeb
giannivitale <giavitale78@gmail.com>
2025-11-11 11:21:16 +0100

    Initiate the repo and adding the list of initialMovies

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// Dati iniziali dei film
+
+const initialMovies = [
+
+  { id: 1, title: 'Inception', genre: 'Fantascienza' },
+  { id: 2, title: 'Il Padrino', genre: 'Thriller' },
+  { id: 3, title: 'Titanic', genre: 'Romantico' },
+  { id: 4, title: 'Batman', genre: 'Azione' },
+  { id: 5, title: 'Interstellar', genre: 'Fantascienza' },
+  { id: 6, title: 'Pulp Fiction', genre: 'Thriller' },
+];
+
+function MovieFilter() {
+
+
+
+
+
+
+}
+
+
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
